0e529ac1f9d1cf12113d82d30317172599aeb8bf
Basmala239 <abuziedbasmala@gmail.com>
2025-12-30 14:05:55 +0200

    added folders

--- ticTacToeServer/src/main/java/module-info.java
+++ ticTacToeServer/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.mycompany.tictactoeserver {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
+    requires java.sql;
 
     opens com.mycompany.tictactoeserver to javafx.fxml;
     exports com.mycompany.tictactoeserver;
