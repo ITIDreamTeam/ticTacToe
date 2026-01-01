3bc5cbca6c2920f928bd3793217fe5512b6866ad
Basmala239 <abuziedbasmala@gmail.com>
2026-01-01 19:13:40 +0200

    update home and add popup

--- ticTacToeClient/src/main/java/module-info.java
+++ ticTacToeClient/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.mycompany.tictactoeclient {
     requires java.base;
     requires java.sql;
     opens com.mycompany.tictactoeclient.presentation.features.home to javafx.fxml;
+
     opens com.mycompany.tictactoeclient to javafx.fxml;
     exports com.mycompany.tictactoeclient;
 }
