88f1753216258265d1139264d3efe0ce0006f9d7
Basmala239 <abuziedbasmala@gmail.com>
2026-01-05 16:58:08 +0200

    update server handler

---
+++ ticTacToeServer/src/main/java/com/mycompany/tictactoeserver/data/model/Message.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tictactoeserver.data.model;
+
+/**
+ *
+ * @author Basmala
+ */
+public class Message {
+
+    private String type;
+    private Object data;
+    private String playerId;
+
+    public Message() {
+    }
+
+    public Message(String type, Object data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(String playerId) {
+        this.playerId = playerId;
+    }
+}
