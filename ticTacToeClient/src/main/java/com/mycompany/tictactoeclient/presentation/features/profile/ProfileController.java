fcad1db747ed242a668a019916d1372372d6bd72
Basmala239 <153266786+Basmala239@users.noreply.github.com>
2026-01-11 06:44:27 +0200

    Remove unused buttons and icon handling in ProfileController
    remove edit buttons

--- ticTacToeClient/src/main/java/com/mycompany/tictactoeclient/presentation/features/profile/ProfileController.java
+++ ticTacToeClient/src/main/java/com/mycompany/tictactoeclient/presentation/features/profile/ProfileController.java
@@ -10,8 +10,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class ProfileController implements Initializable {
 
@@ -20,83 +18,21 @@ public class ProfileController implements Initializable {
     @FXML
     private Button changePassBtn;
     @FXML
-    private Button usernameEditIconBtn;
-    @FXML
-    private Button emailEditIconBtn;
-    @FXML
     private TextField usernameField;
     @FXML
     private TextField emailField;
     @FXML
     private TextField scoreTextField;
-
-    @FXML
-    private Button viewRecordedGamesBtn;
-    private Image editIcon;
-    private Image saveEditIcon;
-    @FXML
-    private ImageView usernameEditIcon;
-    @FXML
-    private ImageView emailEditIcon;
+    
+    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         usernameField.setText(UserSession.getInstance().getUsername());
         emailField.setText(UserSession.getInstance().getEmail());
         scoreTextField.setText(UserSession.getInstance().getScore()+"");
-        try {
-            editIcon = new Image(getClass().getResource("/icons/Edit-20.png").toExternalForm());
-            saveEditIcon = new Image(getClass().getResource("/icons/Save-edit-01.png").toExternalForm());
-        } catch (Exception e) {
-            System.err.println("Error loading icons: " + e.getMessage());
-        }
     }
 
-    private void toggleEditMode(TextField field, Button btn, ImageView icon) {
-        if (!field.isEditable()) {
-            field.setEditable(true);
-            field.getStyleClass().remove("text-field-readonly");
-            field.getStyleClass().add("text-field-editing");
-            field.requestFocus();
-            field.positionCaret(
-                field.getText() == null ? 0 : field.getText().length()
-            );
-            if (saveEditIcon != null) {
-                icon.setImage(saveEditIcon);
-            }
-        } else {
-            if (isInputValid(field)) {
-                field.setEditable(false);
-                field.getStyleClass().remove("text-field-editing");
-                field.getStyleClass().add("text-field-readonly");
-                
-                String newValue = field.getText();
-                if (field == usernameField) {
-                    UserSession.getInstance().setUsername(newValue);
-                    //send to database
-                } else if (field == emailField) {
-                    UserSession.getInstance().setEmail(newValue);
-                }
-                if (editIcon != null) {
-                    icon.setImage(editIcon);
-                }
-
-                System.out.println("Saving new value: " + field.getText());
-            }
-        }
-
-    }
-
-    @FXML
-    private void onEditUsername(ActionEvent event) {
-        toggleEditMode(usernameField, usernameEditIconBtn, usernameEditIcon);
-
-    }
-
-    @FXML
-    private void onEditEmail(ActionEvent event) {
-        toggleEditMode(emailField, emailEditIconBtn, emailEditIcon);
-    }
 
     @FXML
     private void onBackClicked(ActionEvent event) {
